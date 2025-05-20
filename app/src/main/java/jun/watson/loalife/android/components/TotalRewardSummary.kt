e14d89eb7c22dd7aaaf5d8c5083ed1fbf937fbf9
Kim Joon <meaninglessw@gamil.com>
2025-05-20 17:46:19 +0900

    refactor: 불필요한 import문 제거

--- app/src/main/java/jun/watson/loalife/android/components/TotalRewardSummary.kt
+++ app/src/main/java/jun/watson/loalife/android/components/TotalRewardSummary.kt
@@ -1,28 +1,32 @@
 package jun.watson.loalife.android.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import jun.watson.R
 import jun.watson.loalife.android.model.data.Item
-import jun.watson.loalife.android.model.dto.Resource
-import jun.watson.loalife.android.model.dto.CharacterResponseDto
-import jun.watson.loalife.android.model.data.ChaosDungeon
-import jun.watson.loalife.android.model.data.Guardian
 import jun.watson.loalife.android.model.data.Raid
 import jun.watson.loalife.android.model.data.RewardCalculator
+import jun.watson.loalife.android.model.dto.CharacterResponseDto
+import jun.watson.loalife.android.model.dto.Resource
 
 @Composable
 fun TotalRewardSummary(
